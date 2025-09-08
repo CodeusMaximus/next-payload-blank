0dff50ca6f1365a545efc6ed87cfeaf38a62504f
Codeusmaximus <dfsturge@gmail.com>
2025-09-08 06:58:13 -0400

    prod Build

--- app/(app)/track/[shortId]/page.tsx
+++ app/(app)/track/[shortId]/page.tsx
@@ -1,7 +1,15 @@
-/*import type { PageProps } from 'next' // if available in your version
+ // app/(app)/track/[shortId]/page.tsx
 import Client from './track-client'
 
-export default async function TrackPage({ params }: PageProps) {
+export const dynamic = 'force-dynamic' // optional, but good if you live-subscribe
+
+type Params = { shortId: string }
+
+export default async function TrackPage({
+  params,
+}: {
+  params: Promise<Params> // <-- params is a Promise in your setup
+}) {
   const { shortId } = await params
   return <Client shortId={shortId} />
-}*/
+}
